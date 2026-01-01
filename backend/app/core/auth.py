28240dc7b1cf1c38f422322cf7ea3dc79507037f
Shahil <shahilshrestha982@gmail.com>
2026-01-01 21:26:45 +0545

    added code2

---
+++ backend/app/core/auth.py
@@ -0,0 +1,57 @@
+import httpx
+from fastapi import Depends, HTTPException, Request, status
+from clerk_backend_api.security import AuthenticateRequestOptions
+from app.core.config import settings
+from app.core.clerk import clerk
+
+# frontend(jwt token from clerk)->backend
+# backend_authenticate_this token
+# get the user details from clerk
+# check the users permission
+
+
+class AuthUser:
+    def __init__(self,user_id:str,org_id:str,org_permissions:list):
+        self.user_id=user_id
+        self.org_id=org_id
+        self.org_permissions=org_permissions
+
+    def has_permission(self,permission:str)->bool:
+        return permission in self.org_permissions
+    
+    @property
+    def can_view(self)->bool:
+        return self.has_permission("org:tasks:view")
+    
+    @property
+    def can_create(self)->bool:
+        return self.has_permission("org:tasks:create")
+    
+    @property
+    def can_edit(self)->bool:
+        return self.has_permission("org:tasks:edit")
+    
+    @property
+    def can_delete(self)->bool:
+        return self.has_permission("org:tasks:delete")
+    
+
+#frontend sends to backend and backend sends to clerk for validation
+def convert_to_httpx_request(fastapi_request:Request)->httpx.Request:
+    return httpx.Request(
+        method=fastapi_request.method,
+        url=str(fastapi_request.url),
+        headers=dict(fastapi_request.headers)
+    )
+async def get_current_user(request:Request)->AuthUser:
+    httpx_request=convert_to_httpx_request(request)
+
+    request_state=clerk.authenticate_request(
+        httpx_request,
+        AuthenticateRequestOptions(authorized_parties=[settings.Frontend_URL])
+    )
+
+    if not request_state.is_signed_in:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNA
+        )
